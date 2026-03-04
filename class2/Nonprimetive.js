3b9d99ccb5feed60454fa0561aa44272da59b22c
Rajesh39996 <rajesh39996@gamil.com>
2026-03-04 18:03:47 +0530

    hello

--- class2/Nonprimetive.js
+++ class2/Nonprimetive.js
@@ -1,8 +1,8 @@
 let s=[38,40,42,44];
 let colour=[r,b,y,g];
-let emp{
-    "eid":101;
-    "ename":"rajesh";
+let emp={
+    eid: 101,
+    ename:"rajesh" 
 }
 console.log(s);
 console.log(colour);
---
+++ class2/bigintDT.js
@@ -0,0 +1,2 @@
+let a=1234567890n;
+console.log(typeof(a));
