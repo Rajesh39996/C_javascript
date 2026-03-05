5b288fa92bff1c12d540e16d6717ead219e69ddd
Rajesh39996 <rajesh39996@gamil.com>
2026-03-05 10:46:13 +0530

    updated file

--- class4/operators.js
+++ class4/operators.js
@@ -2,7 +2,7 @@ let a=10;
 let b="40";
 let c=true;
 let d=false;
-
+let e;
 
 console.log(a+b);
 console.log(a*b);
@@ -10,3 +10,4 @@ console.log(a+c);
 console.log(a+d);
 console.log(a*c);
 console.log(a*d);
+console.log(a+e);
